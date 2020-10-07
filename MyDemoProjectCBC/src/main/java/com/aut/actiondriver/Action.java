0ae6764e53ad3d8f63b9598531a1df5bd4843f3a
pranitpingale <pranit.pingale01@gmail.com>
2020-10-07 11:35:26 +0530

    First Commit

---
+++ MyDemoProjectCBC/src/main/java/com/aut/actiondriver/Action.java
@@ -0,0 +1,23 @@
+package com.aut.actiondriver;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import com.aut.base.BaseClass;
+
+public class Action extends BaseClass{
+	
+public static void scrollByVisibilityOfElement(WebDriver driver, WebElement ele) {
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+	js.executeScript("arguments[0].scrollIntoView();", ele);
+}
+
+public static void click(WebDriver ldriver, WebElement locatorName) {
+	Actions act = new Actions(ldriver);
+	act.moveToElement(locatorName).click().build().perform();
+}
+
+
+}
---
+++ MyDemoProjectCBC/src/main/java/com/aut/pageobjects/LoginPage.java
@@ -0,0 +1,5 @@
+package com.aut.pageobjects;
+
+public class LoginPage {
+
+}
